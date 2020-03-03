21fcebd856684b6632669c176263490c65a420a6
Viktar_Mikhalevich <v.v.mikhalevich@gmail.com>
2020-03-03 20:07:44 +0300

    Add some method!!!

--- src/main/java/readFile/ParallelFileProcessor.java
+++ src/main/java/readFile/ParallelFileProcessor.java
@@ -1,14 +1,10 @@
 package readFile;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -17,17 +13,21 @@ import java.util.Queue;
 @Component
 public class ParallelFileProcessor implements FileProcessor, Runnable {
 
-
-
-
     //file reader
-    //
+    Map<String, Integer> countOfWords = new HashMap<>();
+    Queue<String> queue = new LinkedList<>();
 
     @Override
     public Map<String, Integer> countWordsInFile(String name) throws IOException {
+        readWordsInFile(name);
+        countOfWords.clear();
+        countWordsFromQueue(countOfWords, queue);
+        System.out.println(countOfWords);
+        return countOfWords;
+    }
+
+    public  Queue<String> readWordsInFile(String name) throws IOException {
         String thisLine;
-        Map<String, Integer> CountOfWords = new HashMap<>();
-        Queue<String> queue = new LinkedList<>();
 
         // read file by other line
         FileReader reader = new FileReader(name);
@@ -45,39 +45,30 @@ public class ParallelFileProcessor implements FileProcessor, Runnable {
                         String newWord = word.toUpperCase();
                         String letter = newWord.substring(0, 1);
                         queue.offer(newWord);
-
-
-
                     }
                 }
-
             }
-
         } catch (IOException e) {
             System.out.println("Don't read file!!!");
             e.printStackTrace();
         } finally {
-            System.out.println("End of work java!!!");
-            System.out.println(CountOfWords);
+            System.out.println("End of read file!!!");
+            System.out.println(countOfWords);
         }
-
-        //New code!!!
-        countWordsFromQueue(CountOfWords, queue);
-
-        return CountOfWords;
+        return queue;
     }
 
     private void countWordsFromQueue(Map<String, Integer> countOfWords, Queue<String> queue) {
         while (!queue.isEmpty()) {
 
+
             String newWord = queue.poll();
             String letter = newWord.substring(0, 1);
-
 
             boolean isLetterInText = false;
 
             for (String key : countOfWords.keySet()) {
-                System.out.println("Key: " + key);
+                //System.out.println("Key: " + key);
 
                 if (letter.equals(key)) {
                     int value = countOfWords.get(key);
@@ -93,9 +84,9 @@ public class ParallelFileProcessor implements FileProcessor, Runnable {
         }
     }
 
-
     @Override
     public void run() {
+        //New code!!!
 
     }
 }
