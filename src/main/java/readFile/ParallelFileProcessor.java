525ae59e68443eb9986a38416a0ae13caa5c465e
Viktar_Mikhalevich <v.v.mikhalevich@gmail.com>
2020-03-03 19:36:20 +0300

    Add methods for read file!

--- src/main/java/readFile/ParallelFileProcessor.java
+++ src/main/java/readFile/ParallelFileProcessor.java
@@ -7,11 +7,15 @@ import org.springframework.stereotype.Component;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 @Component
-public class ParallelFileProcessor implements FileProcessor {
+public class ParallelFileProcessor implements FileProcessor, Runnable {
 
 
 
@@ -23,6 +27,7 @@ public class ParallelFileProcessor implements FileProcessor {
     public Map<String, Integer> countWordsInFile(String name) throws IOException {
         String thisLine;
         Map<String, Integer> CountOfWords = new HashMap<>();
+        Queue<String> queue = new LinkedList<>();
 
         // read file by other line
         FileReader reader = new FileReader(name);
@@ -39,29 +44,15 @@ public class ParallelFileProcessor implements FileProcessor {
                         // folder
                         String newWord = word.toUpperCase();
                         String letter = newWord.substring(0, 1);
+                        queue.offer(newWord);
 
-                        //New code!!!
-                        boolean isLetterInText = false;
-
-                        for (String key : CountOfWords.keySet()) {
-                            System.out.println("Key: " + key);
-
-                            if (letter.equals(key)) {
-                                int value = CountOfWords.get(key);
-                                CountOfWords.put(key, value + 1);
-                                isLetterInText = true;
-                            }
-                        }
-
-                        if (!isLetterInText) {
-                            CountOfWords.put(letter, 1);
-                        }
 
 
                     }
                 }
 
             }
+
         } catch (IOException e) {
             System.out.println("Don't read file!!!");
             e.printStackTrace();
@@ -69,6 +60,42 @@ public class ParallelFileProcessor implements FileProcessor {
             System.out.println("End of work java!!!");
             System.out.println(CountOfWords);
         }
+
+        //New code!!!
+        countWordsFromQueue(CountOfWords, queue);
+
         return CountOfWords;
+    }
+
+    private void countWordsFromQueue(Map<String, Integer> countOfWords, Queue<String> queue) {
+        while (!queue.isEmpty()) {
+
+            String newWord = queue.poll();
+            String letter = newWord.substring(0, 1);
+
+
+            boolean isLetterInText = false;
+
+            for (String key : countOfWords.keySet()) {
+                System.out.println("Key: " + key);
+
+                if (letter.equals(key)) {
+                    int value = countOfWords.get(key);
+                    countOfWords.put(key, value + 1);
+                    isLetterInText = true;
+                }
+            }
+
+            if (!isLetterInText) {
+                countOfWords.put(letter, 1);
+            }
+
+        }
+    }
+
+
+    @Override
+    public void run() {
+
     }
 }
